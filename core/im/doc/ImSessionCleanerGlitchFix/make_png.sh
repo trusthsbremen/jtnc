0db70038bb0e0454572d9e606d5f2d3563829243
Carl-Heinz Genzel <carl-heinz.genzel@hs-bremen.de>
2015-04-14 23:58:17 +0200

    Small changes in build environment.

--- core/im/doc/ImSessionCleanerGlitchFix/make_png.sh
+++ core/im/doc/ImSessionCleanerGlitchFix/make_png.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 dot -Timap -oDefaultImSessionManager.Clean.map DefaultImSessionManager.Clean.dot
+dot -Tcmapx -oDefaultImSessionManager.Clean.mapx DefaultImSessionManager.Clean.dot
 dot -Tpng -oDefaultImSessionManager.Clean.png DefaultImSessionManager.Clean.dot 
